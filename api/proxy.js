88f3e57e75a2385c56d64009489fc7b91f18ae04
Bigroad1314 <135503613+Bigroad1314@users.noreply.github.com>
2024-07-27 14:59:03 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://poe.com/";//your website url
+  let target = "https://chat.openai.com/chat/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
